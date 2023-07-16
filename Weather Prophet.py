31ed644dd6703a5c888605d5e875417d472fb5ad
ZenSam7 <93486188+ZenSam7@users.noreply.github.com>
2023-07-16 20:45:46 +0300

    Added normalization of input data, removed LSTM
    Made the supply and output of data in the form of batches shifting in time (by 1 vector (record) each new step)

--- Weather Prophet.py
+++ Weather Prophet.py
@@ -4,7 +4,7 @@ from keras.layers import Flatten, Dense, SimpleRNN, LSTM, BatchNormalization, Co
 import tensorflow as tf
 from time import time
 import numpy as np
-from Weather_Data import get_moscow_data, get_plank_history, get_weather_history
+from Weather_Data import get_moscow_data, get_plank_history, get_weather_history, print_ai_answers
 
 # Убираем предупреждения
 import os
@@ -12,66 +12,6 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import logging
 tf.get_logger().setLevel(logging.ERROR)
 
-
-# # Работаем с GPU
-# tf.config.set_visible_devices(tf.config.list_physical_devices('GPU'), 'GPU')
-# Работаем с CPU
-tf.config.set_visible_devices([], 'GPU')
-
-
-def print_ai_ans(sequence_len=50, print_real_data=True, print_ai_pred=True,
-                 print_ai_pred_ai_pred=True, print_errors=True, print_mean_error=True,
-                 learning_cycle=0):
-    """ Отображаем предсказания ИИшек, и правильные ответ"""
-
-    real_data, ai_predict, ai_pred_on_ai_pred, error = [], [], [], []
-    rand = np.random.randint(0, len(test_data) - sequence_len - 1)
-
-    # Проверяем на тестовых данных
-    for data in test_data[rand: rand + sequence_len]:
-        real_data.append(data.tolist()[0])
-
-        ai_pred = [i[0, 0, 0] for i in ai.predict(np.resize(data, (1, 1, 7)), verbose=False)]
-        ai_pred = [round(ai_pred[i] + real_data[-1][i + 3], 1) for i in range(4)]
-        ai_predict.append(ai_pred)
-
-        # На сколько % ошиблась нейронка, по сравнению с реальными данными
-        err = []
-        for ans_data, ai_data in zip(real_data[-1][3:], ai_pred):
-            ans_data, ai_data = abs(ans_data), abs(ai_data)
-            err.append(abs(ans_data - ai_data) / (ans_data + 20) * 100)
-        error.append([round(e, 1) for e in err])
-
-        # Скармливаем ИИшке своё же предсказание
-        ai_on_ai = [round(i[0, 0, 0], 1) for i in ai.predict(
-            np.resize(real_data[-1][:3] + ai_pred, (1, 1, 7)), verbose=False)]
-        ai_on_ai = [round(ai_on_ai[i] + real_data[-1][i + 3], 1) for i in range(4)]
-        ai_pred_on_ai_pred.append(ai_on_ai)
-
-    # Выводим данные
-    if print_real_data:
-        print("Time \t\t\t\t Real Data", end="")
-    if print_ai_pred:
-        print(" \t\t\t\t\t\t Ai Predict", end="")
-    if print_ai_pred_ai_pred:
-        print("\t\t\t\t AI data from AI", end="")
-    if print_mean_error:
-        print("\t\t\t Error AI")
-
-    for real, pred, pred_on_pred, err in zip(real_data, ai_predict, ai_pred_on_ai_pred, error):
-        real = [round(i, 1) for i in real]
-        if print_real_data:
-            print(real[:3], "\t", real[3:], end="")
-        if print_ai_pred:
-            print("\t", pred, end="")
-        if print_ai_pred_ai_pred:
-            print("\t", pred_on_pred, end="")
-        if print_errors:
-            print("\t", err, "%")
-
-    if print_mean_error:
-        print(f"{learning_cycle}:", "Mean Error:",
-              round(sum([sum(e) for e in error]) / (4 * len(error)), 1))
 
 
 """Загружаем данные"""
@@ -83,16 +23,20 @@ def print_ai_ans(sequence_len=50, print_real_data=True, print_ai_pred=True,
 
 # ВНИМАНИЕ!: get_moscow_data отличается от get_plank_history и get_weather_history тем,
 # что последнее значение это облачность (%)
-DATA_out = get_plank_history() + get_weather_history()
+DATA_out = get_weather_history()
 print(">>> Dataset loaded\n")
 
 
+
 """Создаём ИИшки"""
+BATCH_SIZE = 50  # Размер батча, который мы подаём и выводим из ИИшки
+# (Отличается от размера батча в ai.fit())
+
 # Суть в том, чтобы расперелить задачи по предсказыванию между разными нейронками
 # Т.к. одна нейросеть очень плохо предскаывает одновременно все факторы
 
 # У всех нейронок одна архитектура и один вход
-input_layer = keras.Input((1, 7))
+input_layer = keras.Input((BATCH_SIZE, 7))
 
 
 def get_ai(name):
@@ -104,20 +48,14 @@ def get_ai(name):
         Conv1D(32, 7, padding="same"),
         BatchNormalization(),
 
-        Dense(50, activation="relu"),
+        Dense(64, activation="tanh"),
         BatchNormalization(),
-        LSTM(50, return_sequences=True, unroll=True),
+        Dense(64, activation="tanh"),
         BatchNormalization(),
-        Dense(50, activation="relu"),
-        BatchNormalization(),
-        LSTM(50, return_sequences=True, unroll=True),
-        BatchNormalization(),
-        Dense(50, activation="relu"),
-
-        Dense(1, activation="linear"),
+        Dense(64, activation="tanh"),
     ])(input_layer)
 
-    output = Dense(1, activation="linear", name=name)(model)
+    output = Dense(1, activation="tanh", name=name)(model)
 
     return output
 
@@ -129,67 +67,62 @@ cloud_or_wind = get_ai("cloud_wind")  # cloud or wind
 
 
 ai = keras.Model(input_layer, [temperature, pressure, humidity, cloud_or_wind])
-ai.compile(optimizer=keras.optimizers.Adagrad(0.001), loss="mean_squared_error",
-           loss_weights={"temp": 10.0, "press": 1.0, "humid": 1.0, "cloud_wind": 1.0})
+ai.compile(optimizer=keras.optimizers.Adagrad(0.001), loss="mean_absolute_error",
+           loss_weights={"temp": 100.0, "press": 10.0, "humid": 10.0, "cloud_wind": 10.0})
            # Отдаём приоритет температуре
 
-# ai.summary()
+ai.summary(); print()
+
 
 
 """Сохранения / Загрузки"""
 def save_path(name): return f"Saves Weather Prophet/{name}"
 
-def SAVE_NAME(num): return f"5_save~{num}"
+def SAVE_NAME(num): return f"AI_v2.0~{num}"
 
 # Как загружать: ai = tf.keras.models.load_model(save_path(AI_NAME))
 # Как сохранять: ai.save(save_path(AI_NAME))
 
-# ЗАГРУЖАЕМСЯ
-print(f">>> Loading the {SAVE_NAME(3)}", end="\t\t")
-ai = tf.keras.models.load_model(save_path(SAVE_NAME(3)))
-print("Done\n")
 
 
 """DATA_in == Данные погоды, начиная с 1ого дня (принимает)
    DATA_out == Данные погоды, начиная с 2ого дня (должен предсказать)"""
 
-# Создаём смещени назад во времени
-DATA_in = DATA_out[:-1]
-DATA_out = DATA_out[1:]
+# Создаём смещени назад во времени и изменяем размер так,
+# чтобы можно было подать сразу BATCH_SIZE веркторов
+# (это позволяет не использовать RNN, ведь подаём мы последние BATCH_SIZE записей)
+DATA_in = DATA_out[:-1][: len(DATA_out) // BATCH_SIZE * BATCH_SIZE]
+DATA_out = DATA_out[1:][: len(DATA_out) // BATCH_SIZE * BATCH_SIZE]
 
-DATA_out = np.array(DATA_out).reshape((len(DATA_out), 1, 7))
-DATA_in = np.array(DATA_in).reshape((len(DATA_out), 1, 7))
+DATA_in = np.array([DATA_in[i: i +BATCH_SIZE] for i in range(len(DATA_out) - BATCH_SIZE)])
+DATA_out = np.array([DATA_out[i: i +BATCH_SIZE] for i in range(len(DATA_out) - BATCH_SIZE)])[:, :, 3:]
 
-DATA_out = DATA_out - DATA_in   # Остаточное обучение
-DATA_out = DATA_out[:, :, 3:]   # (ИИшке не надо предсказывать время)
 
 
 """Обучение"""
-
-callbacks = [
-    #keras.callbacks.EarlyStopping(monitor="loss", min_delta=0, patience=5, verbose=False),
-]
-
-# Берём % от входных данных
-test_size = int(len(DATA_in) * 0.01)
+# % от всех данных
+test_size = int(len(DATA_in) * 0.05)
 
 # Разделяем часть для обучения и для тестирования
 # В качестве ответа записываем значение природного явления
 train_data = DATA_in[:-test_size]
-train_data_answer = np.reshape(np.array([DATA_out[:-test_size, 0, :]]), (len(train_data), 1, 4))
+train_data_answer = DATA_out[:-test_size]
 
 test_data = DATA_in[-test_size:]
-test_data_answer = np.reshape(np.array([DATA_out[-test_size:, 0, :]]), (test_size, 1, 4))
+test_data_answer = [DATA_out[-test_size:]]
 
 
-for learning_cycle in range(6, 99):
+for learning_cycle in range(1, 99):
+    ЗАГРУЖАЕМСЯ
+    print(f">>> Loading the {SAVE_NAME(learning_cycle)}", end="\t\t")
+    ai = tf.keras.models.load_model(save_path(SAVE_NAME(learning_cycle)))
+    print("Done")
+    learning_cycle += 1
+
+
     print(f">>> Learning the {SAVE_NAME(learning_cycle)}")
 
-    ai.fit(train_data, train_data_answer, epochs=4, batch_size=50, verbose=False,
-           shuffle=False, callbacks=callbacks)
-
-    # print(">>> Testing:")
-    # ai.evaluate(test_data, test_data_answer, batch_size=10, verbose=True)
+    ai.fit(train_data, train_data_answer, epochs=5, batch_size=150, verbose=True, shuffle=False)
 
     print("\n")
 
@@ -199,5 +132,5 @@ for learning_cycle in range(6, 99):
     ai.save(save_path(SAVE_NAME(learning_cycle)))
     print("Done (Ignore the WARNING)\n")
 
-
-    print_ai_ans(50, True, True, False, True, True)
+    # Выфводим данные и сравниваем их "на глаз"
+    print_ai_answers(ai, test_data, learning_cycle, BATCH_SIZE)
--- Weather_Data.py
+++ Weather_Data.py
@@ -1,3 +1,64 @@
+from numpy import tanh, arctanh
+import numpy as np
+
+
+def norm_temperature(x, convert_back=False):
+    """Нормализуем температуру от -1 до 1"""
+    if convert_back:
+        return round(arctanh(x)*20, 1)
+
+    return tanh(x/20)
+
+def norm_pressure(x, convert_back=False):
+    """Нормализуем давление от -1 до 1"""
+    if convert_back:
+        return round(arctanh(x) *15 +755, 1)
+
+    return tanh( (x -755)/15 )
+
+def norm_humidity(x, convert_back=False):
+    """Нормализуем влажность от -1 до 1"""
+    if convert_back:
+        return int(x *50 +50)
+
+    return (x -50)/50
+
+def norm_cloud(x, convert_back=False):
+    """Нормализуем облачность от -1 до 1"""
+    if convert_back:
+        return int(x *50 +50)
+
+    return (x -50)/50
+
+def norm_wind(x, convert_back=False):
+    """Нормализуем скорость ветра от -1 до 1"""
+    if convert_back:
+        return round(arctanh(x) *2 +2, 1)
+
+    return tanh((x -2) /2)
+
+def norm_hours(x, convert_back=False):
+    """Нормализуем время суток от -1 до 1"""
+    if convert_back:
+        return int(x*12 +12)
+
+    return (x -12)/12
+
+def norm_day(x, convert_back=False):
+    """Нормализуем номер дня от -1 до 1"""
+    if convert_back:
+        return int(x *15.5 +15.5)
+
+    return (x -15.5) /15.5
+
+def norm_month(x, convert_back=False):
+    """Нормализуем номер месяца от -1 до 1"""
+    if convert_back:
+        return int(x *6 +6)
+
+    return (x -6)/6
+
+
 
 
 
@@ -36,13 +97,13 @@ def get_moscow_data():
                 # data[0] -> часы (в течении дня)
                 # datap[1] -> день
                 # data[2] -> месяц
-                processed_data[0] = int(data[0][11:13])
-                processed_data[1] = int(data[0][:2])
-                processed_data[2] = int(data[0][3:5])
-                processed_data[3] = float(data[1].replace(",", "."))
-                processed_data[4] = float(data[2].replace(",", "."))
-                processed_data[5] = int(data[3])
-                processed_data[6] = int(data[4])
+                processed_data[0] = norm_hours(int(data[0][11:13]))
+                processed_data[1] = norm_day(int(data[0][:2]))
+                processed_data[2] = norm_month(int(data[0][3:5]))
+                processed_data[3] = norm_temperature(float(data[1].replace(",", ".")))
+                processed_data[4] = norm_pressure(float(data[2].replace(",", ".")))
+                processed_data[5] = norm_humidity(int(data[3]))
+                processed_data[6] = norm_cloud(int(data[4]))
 
                 DATA.append(processed_data)
     return DATA
@@ -88,13 +149,13 @@ def get_plank_history():
 
             processed_data = [0 for _ in range(7)]
 
-            processed_data[0] = int(data[0][11:13]) + int(data[0][14:16]) /60
-            processed_data[1] = int(data[0][:2])
-            processed_data[2] = int(data[0][3:5])
-            processed_data[3] = float(data[2])
-            processed_data[4] = float(data[1]) *100 / 133.322
-            processed_data[5] = float(data[5])
-            processed_data[6] = float(data[12]) *10
+            processed_data[0] = norm_hours(int(data[0][11:13]) + int(data[0][14:16]) /60)
+            processed_data[1] = norm_day(int(data[0][:2]))
+            processed_data[2] = norm_month(int(data[0][3:5]))
+            processed_data[3] = norm_temperature(float(data[2]))
+            processed_data[4] = norm_pressure(float(data[1]) *100 / 133.322)
+            processed_data[5] = norm_humidity(float(data[5]))
+            processed_data[6] = norm_wind(float(data[12]) *10)
 
             DATA.append(processed_data)
     return DATA
@@ -137,13 +198,93 @@ def get_weather_history():
 
             processed_data = [0 for _ in range(7)]
 
-            processed_data[0] = int(data[0][11:13])
-            processed_data[1] = int(data[0][8:10])
-            processed_data[2] = int(data[0][5:7])
-            processed_data[3] = float(data[3])
-            processed_data[4] = float(data[10]) * 0.750063755419211
-            processed_data[5] = float(data[5]) * 100
-            processed_data[6] = float(data[6]) * 1_000 / 3_600
+            processed_data[0] = norm_hours(int(data[0][11:13]))
+            processed_data[1] = norm_day(int(data[0][8:10]))
+            processed_data[2] = norm_month(int(data[0][5:7]))
+            processed_data[3] = norm_temperature(float(data[3]))
+            processed_data[4] = norm_pressure(float(data[10]) * 0.750063755419211)
+            processed_data[5] = norm_humidity(float(data[5]) * 100)
+            processed_data[6] = norm_wind(float(data[6]) * 1_000 / 3_600)
 
             DATA.append(processed_data)
     return DATA
+
+
+
+
+def print_ai_answers(ai, real_data, learning_cycle, batch_size):
+    print("\n")
+    print("Time\t\t\tReal Data\t\t\t\t\tAI answer\t\t\t\t\tAI answer on AI\t\t\t\tErrors ∆")
+
+    # Случайный батч
+    real_matrix = np.array([real_data[np.random.randint(len(real_data))]])
+
+    real_data_list = real_matrix.tolist()[0]
+    ai_ans_list = np.reshape(np.array(ai.predict(real_matrix, verbose=False)), (batch_size, 4)).tolist()
+
+    ai_ans_with_time = np.array( [time + ans for time, ans in zip(
+                                 [time[:3] for time in real_data_list], ai_ans_list)])
+    ai_ans_on_ai_list = ai.predict( np.reshape(ai_ans_with_time, (1, batch_size, 7)), verbose=False)
+    ai_ans_on_ai_list = np.reshape(np.array(ai_ans_on_ai_list), (batch_size, 4)).tolist()
+
+
+    # Конвертируем данные из промежутка [-1; 1] в нормальную физическую величину
+    converted_real_data = []
+    for record in real_data_list:
+        converted_real_data.append([
+            norm_hours(record[0], True),
+            norm_day(record[1], True),
+            norm_month(record[2], True),
+            norm_temperature(record[3], True),
+            norm_pressure(record[4], True),
+            norm_humidity(record[5], True),
+            norm_wind(record[6], True),
+        ])
+
+    converted_ai_ans_list = []
+    for ai_ans in ai_ans_list:
+        converted_ai_ans_list.append([
+            norm_temperature(ai_ans[0], True),
+            norm_pressure(ai_ans[1], True),
+            norm_humidity(ai_ans[2], True),
+            norm_wind(ai_ans[3], True),
+        ])
+
+    converted_ai_ans_on_ai_list = []
+    for ai_ans_on_ai in ai_ans_list:
+        converted_ai_ans_on_ai_list.append([
+            norm_temperature(ai_ans_on_ai[0], True),
+            norm_pressure(ai_ans_on_ai[1], True),
+            norm_humidity(ai_ans_on_ai[2], True),
+            norm_wind(ai_ans_on_ai[3], True),
+        ])
+
+
+    # В качестве ошибки просто добавляем разность между ответом ИИ и реальностью
+    errors = []
+    for real, ai_ans in zip(converted_real_data, converted_ai_ans_list):
+        real = np.array(real[3:])
+        ai_ans = np.array(ai_ans)
+
+        errors.append(np.abs(real - ai_ans))
+    errors = np.array(errors)
+
+
+    # Выводим всё
+    for real, ai_ans, ai_on_ai, err in zip(converted_real_data,
+              converted_ai_ans_list, converted_ai_ans_on_ai_list, errors):
+        print(np.round(np.array(real[:3]),  1), "\t",
+              np.round(np.array(real[3:]),  1), "\t",
+              np.round(np.array(ai_ans),    1), "\t",
+              np.round(np.array(ai_on_ai),  1), "\t",
+              np.round(np.array(err),       1))
+
+
+    print("\nMean errors:",
+          "\n\t Temperature:  ", np.round(np.mean(errors[:, 0]), 1),
+          "\n\t Pressure:     ", np.round(np.mean(errors[:, 1]), 1),
+          "\n\t Humidity:     ", np.round(np.mean(errors[:, 2]), 1),
+          "\n\t Wind:         ", np.round(np.mean(errors[:, 3]), 1),
+          "\n\n\t TOTAL:        ", np.round(np.mean(errors     ),  1))
+
+    print("\n")
